56d4b0e6804723f35c70db8d91b280be4a3ac8ae
Nathan Ribeiro <nathan.ribeiros7@gmail.com>
2022-08-09 15:30:17 -0300

    enviando tarefa JS

---
+++ script.js
@@ -0,0 +1,12 @@
+window.alert('aaaaaaaaaaaaa')
+
+let nome = 'Jhon'
+let sobrenome = 'Doyle'
+let anoNacimento = 1991
+let anoAtual = 2022
+let idade = anoAtual - anoNacimento
+let cidade = 'Curitiba'
+
+let frase = `Eu sou o ${nome} ${sobrenome}, tenho ${idade} anos e moro em ${cidade}` 
+
+console.log(frase)
